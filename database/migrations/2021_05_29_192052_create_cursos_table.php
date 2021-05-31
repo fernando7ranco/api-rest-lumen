2e60d994937846350b6cb8dd9b6a438e6bc41996
franco <fe.7ranco@gmail.com>
2021-05-31 19:00:01 -0300

    tabelas do sistema com migrations

--- database/migrations/2021_05_29_192052_create_cursos_table.php
+++ database/migrations/2021_05_29_192052_create_cursos_table.php
@@ -14,7 +14,8 @@ class CreateCursosTable extends Migration
     public function up()
     {
         Schema::create('cursos', function (Blueprint $table) {
-            $table->id();
+            #$table->id();
+            $table->integer('id')->autoIncrement();
             $table->string('nome');
             $table->string('descricao');
             $table->text('conteudo');
