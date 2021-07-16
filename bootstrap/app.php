d8d8480e9f6e9facfeb871162010c34c79b911e3
franco <fe.7ranco@gmail.com>
2021-07-16 18:18:06 -0300

    cors middleware para acesso publico

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,12 +72,12 @@ $app->configure('app');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
 $app->routeMiddleware([
-     'auth' => App\Http\Middleware\Authenticate::class,
+    'auth' => App\Http\Middleware\Authenticate::class,
 ]);
 
 /*
